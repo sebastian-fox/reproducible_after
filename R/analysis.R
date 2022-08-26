f0d9012d5a2b73050816c43c5ad6922fa00f88eb
Sebastian Fox <sebastian.fox@phe.gov.uk>
2022-08-26 12:25:26 +0100

    analysis is documented

--- R/analysis.R
+++ R/analysis.R
@@ -1,13 +1,16 @@
 source("R/libraries.R")
 
+# obtain the grids for UTLAs in London and outside of London
 grid_london <- EnglandFacets::london_grid
 grid_no_london <- EnglandFacets::outside_london_grid
 
+# obtain the indicator data from the Fingertips API
 df <- fingertipsR::fingertips_data(
         IndicatorID = 92488,
         AreaTypeID = 402
 )
 
+# ensure the field names match between df and the future shp objects
 df <- df |> 
         dplyr::rename(
                 name = AreaName, 
@@ -15,11 +18,12 @@ df <- df |>
         dplyr::mutate(
                 name = abbreviate(name, 6))
 
+# obtain the shape objects for England and London
 shp_eng <- EnglandFacets::england_shp
-
 shp_lond <- EnglandFacets::london_shp
 
 
+# separate out London and non-London data
 england_data <- df |> 
         dplyr::filter(code %in% grid_no_london$code,
                       AreaType == "Counties & UAs (from Apr 2021)",
@@ -28,12 +32,15 @@ london_data <- df |>
         dplyr::filter(code %in% grid_london$code,
                       AreaType == "Counties & UAs (from Apr 2021)",
                       Sex == "Persons")
+
+# create plot
 plot <- EnglandFacets::utla_plot(
         london_data = london_data,
         england_data = england_data,
         london_grid = grid_london,
         england_grid = grid_no_london)
 
+# save plot
 ggplot2::ggsave(plot,
        filename = "report/analysis_map.png",
        height = 13,
